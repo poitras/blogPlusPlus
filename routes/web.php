bd02e5474d37a5e3855944cc88163baf73c114fb
poitras <nicopoitras@gmail.com>
2017-04-11 00:16:17 -0400

    Ajout du workflow des posts et layouts de la vue

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,4 @@
 <?php
 
-use App\Task;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-
-    return view('welcome');
-
-});
-
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
