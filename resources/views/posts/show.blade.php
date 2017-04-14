0c9f140e1018f4b804d6478ce1e8474235d2d7f5
poitras <nicopoitras@gmail.com>
2017-04-13 22:39:57 -0400

    Changement de la vue pour ressembler à un blog et ajout creation de posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Show</h1>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+
+//Route::get('/posts/{post}', 'PostsController@show');
+
+Route::get('/posts/create', 'PostsController@create');
+
+Route::post('/posts', 'PostsController@store');
