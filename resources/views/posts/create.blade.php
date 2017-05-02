4d134f5ef710452a80b8a20688f06c60aa6fa22f
poitras <nicopoitras@gmail.com>
2017-05-02 11:41:24 -0400

    Ajout validation des posts et layouts pour les errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,19 +13,21 @@
 
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control" id="title" name="title" >
             </div>
 
             <div class="form-group">
                 <label for="body">Body</label>
-                <textarea class="form-control" id="body" rows="3" name="body"></textarea>
+                <textarea class="form-control" id="body" rows="3" name="body" ></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Publish</button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Publish</button>
+            </div>
+
+            @include('layouts.errors')
 
         </form>
-
-        <br>
 
     </div>
 @endsection
